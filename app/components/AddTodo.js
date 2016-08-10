d34a6997e4ec9d07a4d1f9d8f5e3f26373fb0d70
Bear <15223245@qq.com>
2016-08-10 18:26:02 +0800

    add  screenCapture

--- app/components/AddTodo.js
+++ app/components/AddTodo.js
@@ -61,6 +61,8 @@ class AddTodo extends Component {
                 })
             }}>
             <View style={{width:100,height:100,backgroundColor: color}}></View>
+
+
           </TouchableHighlight>
           )
         })
